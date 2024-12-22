23ed3dcb650d7ae36410de701e2226c59c62c1f7
shaikarif010 <eng23am0190@dsu.edu.in>
2024-12-22 22:32:56 +0530

    Create Matrix_Multiplication.java

---
+++ Matrix_Multiplication.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows for Matrix A: ");
+        int rowsA = sc.nextInt();
+        System.out.print("Enter number of columns for Matrix A: ");
+        int colsA = sc.nextInt();
+
+        int[][] matrixA = new int[rowsA][colsA];
+
+        System.out.println("Enter elements of Matrix A:");
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsA; j++) {
+                matrixA[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.print("Enter number of rows for Matrix B: ");
+        int rowsB = sc.nextInt();
+        System.out.print("Enter number of columns for Matrix B: ");
+        int colsB = sc.nextInt();
+
+        if (colsA != rowsB) {
+            System.out.println("Matrix multiplication is not possible.");
+            sc.close();
+            return;
+        }
+
+        int[][] matrixB = new int[rowsB][colsB];
+
+        System.out.println("Enter elements of Matrix B:");
+        for (int i = 0; i < rowsB; i++) {
+            for (int j = 0; j < colsB; j++) {
+                matrixB[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] result = new int[rowsA][colsB];
+
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+
+        System.out.println("Resultant Matrix:");
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
