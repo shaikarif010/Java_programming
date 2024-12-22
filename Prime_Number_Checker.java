22093572f2ca9ac79cd65a9c82116ea691e4ec19
shaikarif010 <eng23am0190@dsu.edu.in>
2024-12-22 22:35:14 +0530

    Create Prime_Number_Checker.java

---
+++ Prime_Number_Checker.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrimeNumberChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        boolean isPrime = true;
+        
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        
+        if (isPrime && num > 1) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+
+        sc.close();
+    }
+}
