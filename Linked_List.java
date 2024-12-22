af373c91fddcecdaae3a7efaf3b853acbf28a545
shaikarif010 <eng23am0190@dsu.edu.in>
2024-12-22 22:32:03 +0530

    Create Linked_List.java

---
+++ Linked_List.java
@@ -0,0 +1,53 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class SinglyLinkedList {
+    private Node head;
+
+    public SinglyLinkedList() {
+        this.head = null;
+    }
+
+    // Add a new node at the end
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    // Display the linked list
+    public void display() {
+        if (head == null) {
+            System.out.println("List is empty.");
+        } else {
+            Node current = head;
+            while (current != null) {
+                System.out.print(current.data + " ");
+                current = current.next;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.display();  // Output: 10 20 30
+    }
+}
